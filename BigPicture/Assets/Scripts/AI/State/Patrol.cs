f639b70e5280936d664fb9633b57d69a5e1c04d4
DESKTOP-E27HKCU\leeho <leehoseo@game.hoseo.edu>
2017-03-17 19:58:40 +0900

    State관련 스크립트 만들기 시작 SingleTon , Template

--- BigPicture/Assets/Scripts/AI/State/Patrol.cs
+++ BigPicture/Assets/Scripts/AI/State/Patrol.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class State : MonoBehaviour {
+public class Patrol : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
---
+++ BigPicture/Assets/Scripts/AI/State/State.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State<entity_type> {
+
+    private static State<entity_type> instance;
+
+    public State<entity_type> Instance
+    {
+        get
+        {
+            try
+            {
+                return instance;
+            }
+            catch
+            {
+                instance = new State<entity_type>();
+                return instance;
+            }
+        }
+    }
+
+    public virtual void Exicute(entity_type _entity_type)
+    {
+
+    }
+    public virtual void Enter(entity_type _entity_type)
+    {
+
+    }
+    public virtual void Exit(entity_type _entity_type)
+    {
+
+    }
+}
