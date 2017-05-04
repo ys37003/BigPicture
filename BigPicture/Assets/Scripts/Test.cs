dc2febf2c727eb5082bb669e7457fa0c51a01933
DESKTOP-E27HKCU\leeho <leehoseo@game.hoseo.edu>
2017-05-05 01:26:49 +0900

    no message

---
+++ BigPicture/Assets/Scripts/Test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    public GameObject cube;
+    Vector3 test;
+	// Use this for initialization
+	void Start () {
+        test = this.transform.position - cube.transform.position;
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position += test;
+
+    }
+}
