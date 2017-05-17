5872b3de0dbfb332011a9dd62db6ee34fb18b8e3
Amariris <ys37003@gmail.com>
2017-05-18 04:37:52 +0900

    라이팅 재적용

--- BigPicture/Assets/Scripts/UI/Status/StatusUI.cs
+++ BigPicture/Assets/Scripts/UI/Status/StatusUI.cs
@@ -47,12 +47,12 @@ public class StatusUI : UIBase<StatusUI>
             switch (slider.stat)
             {
                 case eSTAT.STRENGTH: slider.SetData(character.Status.Strength); break;
-                case eSTAT.SPELL: slider.SetData(character.Status.Spell); break;
-                case eSTAT.AGILITY: slider.SetData(character.Status.Agility); break;
-                case eSTAT.AVOID: slider.SetData(character.Status.Avoid); break;
-                case eSTAT.DEFENSE: slider.SetData(character.Status.Defense); break;
+                case eSTAT.SPELL:    slider.SetData(character.Status.Spell);    break;
+                case eSTAT.AGILITY:  slider.SetData(character.Status.Agility);  break;
+                case eSTAT.AVOID:    slider.SetData(character.Status.Avoid);    break;
+                case eSTAT.DEFENSE:  slider.SetData(character.Status.Defense);  break;
                 case eSTAT.RECOVERY: slider.SetData(character.Status.Recovery); break;
-                case eSTAT.LUCK: slider.SetData(character.Status.Luck); break;
+                case eSTAT.LUCK:     slider.SetData(character.Status.Luck);     break;
             }
         }
     }
@@ -62,12 +62,12 @@ public class StatusUI : UIBase<StatusUI>
         switch (stat)
         {
             case eSTAT.STRENGTH: character.Status.Strength += value; break;
-            case eSTAT.SPELL: character.Status.Spell += value; break;
-            case eSTAT.AGILITY: character.Status.Agility += value; break;
-            case eSTAT.AVOID: character.Status.Avoid += value; break;
-            case eSTAT.DEFENSE: character.Status.Defense += value; break;
+            case eSTAT.SPELL:    character.Status.Spell    += value; break;
+            case eSTAT.AGILITY:  character.Status.Agility  += value; break;
+            case eSTAT.AVOID:    character.Status.Avoid    += value; break;
+            case eSTAT.DEFENSE:  character.Status.Defense  += value; break;
             case eSTAT.RECOVERY: character.Status.Recovery += value; break;
-            case eSTAT.LUCK: character.Status.Luck += value; break;
+            case eSTAT.LUCK:     character.Status.Luck     += value; break;
         }
 
         character.SkillPoint -= value;
@@ -80,11 +80,11 @@ public class StatusUI : UIBase<StatusUI>
         labelSkillPoint.text = string.Format("{0}", character.SkillPoint);
 
         labelPhysicsPower.text = string.Format("물리공격력 : {0}", character.Status.PhysicsPower);
-        labelSpellPower.text = string.Format("마법공격력 : {0}", character.Status.SpellPower);
-        labelMoveSpeed.text = string.Format("이동속도 : {0}", character.Status.MoveSpeed);
-        labelEvasionRate.text = string.Format("회피율 : {0}", character.Status.EvasionRate);
-        labelArmor.text = string.Format("방어력 : {0}", character.Status.Armor);
-        labelRecoveryRPS.text = string.Format("초당 회복력 : {0}", character.Status.RecoveryRPS);
+        labelSpellPower.text   = string.Format("마법공격력 : {0}", character.Status.SpellPower);
+        labelMoveSpeed.text    = string.Format("이동속도 : {0}", character.Status.MoveSpeed);
+        labelEvasionRate.text  = string.Format("회피율 : {0}", character.Status.EvasionRate);
+        labelArmor.text        = string.Format("방어력 : {0}", character.Status.Armor);
+        labelRecoveryRPS.text  = string.Format("초당 회복력 : {0}", character.Status.RecoveryRPS);
     }
 
     private int getSkillPoint()
