b716348f22b8ca3eacaa823437d8c7831faa4919
Amariris <ys37003@gmail.com>
2017-06-13 02:20:08 +0900

    World 씬 배치중

---
+++ BigPicture/Assets/Scripts/Manager/CinemaManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinemaManager : Singleton<CinemaManager>
+{
+    public GameObject CinemaPos;
+
+    private void Awake()
+    {
+        if (CinemaPos == null)
+            CinemaPos = GameObject.Find("CinemaPoses");
+    }
+}
