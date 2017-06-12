bff608fe78149fa4216208f9a0364f841e1b1a7b
Amariris <ys37003@gmail.com>
2017-06-13 02:19:38 +0900

    UI 수정

---
+++ BigPicture/Assets/Scripts/UI/Title/TitleUI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class TitleUI : UIBase<TitleUI>
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
